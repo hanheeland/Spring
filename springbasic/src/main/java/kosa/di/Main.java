010020e6482a4e18ef12c37ffb527c373eb6f449
hanheeland <susunnn48@gmail.com>
2024-08-05 18:17:17 +0900

    설정 변경

--- springbasic/src/main/java/kosa/di/Main.java
+++ springbasic/src/main/java/kosa/di/Main.java
@@ -12,7 +12,7 @@ public class Main {
    public static void main(String[] args) {
 
 
-	 // 1. 스프링 컨테이너 생성 (xml 파일로 생성)
+	 // 1. 스프링 컨테이너 생성 (xml 파일로 생성 )
 	 /*
 	 * Resource resource = new ClassPathResource("applicationContext.xml");
 	 * BeanFactory factory = new GenericXmlApplicationContext(resource);
@@ -30,12 +30,12 @@ public class Main {
 	   // 3. 스프링 컨테이너 생성 (aop 파일로 생성)
 	   ApplicationContext factory =
 			   new ClassPathXmlApplicationContext(conFiguration);
-//	   
-//      Service service = (Service)factory.getBean("writeServicey");
+	   
+//      Service service = (Service)factory.getBean("writeService");
 //      service.insertService();
 	   
 	   Kosa kosa = (Kosa)factory.getBean("kosaService");
-	   //kosa.insertService();
-	   kosa.list();
+	   kosa.insertService();
+	   //kosa.list();
    }
 }
