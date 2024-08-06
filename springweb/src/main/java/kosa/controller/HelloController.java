52834e355c6f4ed0f2129782ce1f6e1feb5a8a76
hanheeland <susunnn48@gmail.com>
2024-08-06 13:30:05 +0900

    Board Controller 추가

--- springweb/src/main/java/kosa/controller/HelloController.java
+++ springweb/src/main/java/kosa/controller/HelloController.java
@@ -2,6 +2,7 @@ package kosa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,7 +14,8 @@ public class HelloController {
 	@Autowired
 	private HelloService Service;
 	
-	@RequestMapping("/hello.do")
+	//@RequestMapping("/hello.do")
+	@GetMapping({"/", "/hello"})
 	public ModelAndView hello() {
 		ModelAndView mav = new ModelAndView();
 		//mav.addObject("message", "Hello Spring!!!");
