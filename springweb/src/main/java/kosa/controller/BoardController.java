c93ee08e741764f4a655d1b8eb80427c76c72e85
hanheeland <susunnn48@gmail.com>
2024-08-06 15:20:07 +0900

    Board Mybatis select / insert 작업

--- springweb/src/main/java/kosa/controller/BoardController.java
+++ springweb/src/main/java/kosa/controller/BoardController.java
@@ -1,11 +1,21 @@
 package kosa.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import kosa.model.Board;
+import kosa.model.BoardDao;
+
 @Controller
 public class BoardController {
+	
+	@Autowired
+	private BoardDao dao;
 	
 	//board_insert => insert_form.jsp 출력
 	@GetMapping("/board_insert")
@@ -13,12 +23,19 @@ public class BoardController {
 		return "insert_form";
 	}
 	
-	// 디스패처서블릿을 거쳐서 가야해서 redirect 사용한다.
+	// 중복으로 글을 쓰지 않기 위해 redirect 사용한다.
 	// 다이렉트로 jsp 가면 안된다.
 	@PostMapping("/board_insert")
-	public String board_insert() {
-		System.out.println("호출");
+	public String board_insert(Board board) {
+		dao.insertBoard(board);
 		return "redirect:board_list";
+	}
+	
+	@GetMapping("/board_list")
+	public String board_list(Model model) {
+		List<Board> list = dao.listBoard();
+		model.addAttribute("list", list); // 뷰에서 사용 가능하도록한다.
+		return "list";
 	}
 	
 }
