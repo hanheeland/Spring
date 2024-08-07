df5206c7182d0ca08a94df3a140783e458d8c934
hanheeland <susunnn48@gmail.com>
2024-08-07 13:21:14 +0900

    테스트 코드 샘플 작성

--- spring_work2/ex00/src/test/java/org/zerock/sample/DataSoruceTests.java
+++ spring_work2/ex00/src/test/java/org/zerock/sample/DataSoruceTests.java
@@ -1,29 +1,27 @@
 package org.zerock.sample;
 
 import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.mapper.TimeMapper;
 
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
 @Log4j
-public class SampleTests {
+public class DataSoruceTests {
 	
 	@Autowired
-	private Restaurant restaurant;
-
+	private TimeMapper timeMapper;
+	
 	@Test
 	public void test() {
-		assertNotNull(restaurant);
-		log.info(restaurant.toString());
-		log.info(restaurant.getChef());
+		log.info(timeMapper.getTime());
 	}
 
 }
