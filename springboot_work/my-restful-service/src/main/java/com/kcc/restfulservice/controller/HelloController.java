f60b7cdc24f8f194d4f6f26f7d4afb70149e1af0
hanheeland <susunnn48@gmail.com>
2024-08-19 10:53:59 +0900

    스프링부트 설정

---
+++ springboot_work/my-restful-service/src/main/java/com/kcc/restfulservice/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.kcc.restfulservice.controller;
+
+import com.kcc.restfulservice.bean.HelloWorldBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World Bean!!");
+    }
+}
---
+++ springboot_work/my-restful-service/src/test/java/com/kcc/restfulservice/MyRestfulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcc.restfulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestfulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
