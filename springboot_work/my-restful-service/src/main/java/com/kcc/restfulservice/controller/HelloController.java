6b73bb26c9c68913803637a0ae3561e91812b76b
hanheeland <susunnn48@gmail.com>
2024-08-19 16:08:25 +0900

    다국어 처리

--- springboot_work/my-restful-service/src/main/java/com/kcc/restfulservice/controller/HelloController.java
+++ springboot_work/my-restful-service/src/main/java/com/kcc/restfulservice/controller/HelloController.java
@@ -1,12 +1,20 @@
 package com.kcc.restfulservice.controller;
 
 import com.kcc.restfulservice.bean.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String helloWorld() {
@@ -21,5 +29,15 @@ public class HelloController {
     @GetMapping("/hello-world-bean/{name}")
     public HelloWorldBean helloWorldBean2(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello world bean %s!", name));
+    }
+
+    // 다국어 처리
+    //Accept-language 값을 넘겨야한다.
+    @GetMapping("/hello-world-international")
+    public String helloWorldInternational(
+            @RequestHeader(name="Accept-Language", required = false)
+            Locale locale
+    ) {
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 }
